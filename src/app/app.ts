de3f0a172631f99fa8b7ff007dd34e2334911c72
Clemens Sietas <siecle@gmx.de>
2025-09-12 22:13:42 +0200

    Store injected

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosStore } from './store/todos.store';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('ngrx-signal-store');
+
+  store = inject(TodosStore);
 }
